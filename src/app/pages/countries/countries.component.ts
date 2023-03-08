ea4044b285b5d38ab91a91df8844aca53e898d64
MarkoMiljkovic45 <miljkovicmarko45@gmail.com>
2023-03-08 18:59:20 +0100

    Make app-card-component

--- src/app/pages/countries/countries.component.ts
+++ src/app/pages/countries/countries.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
+import {CountriesService} from "../../services/countries.service";
+import {Country} from "../../models/country.interface";
+import {Observable} from "rxjs";
+import {CommonModule} from "@angular/common";
+import {CountryCardComponent} from "../../components/country-card/country-card.component";
 
 @Component({
   selector: 'app-countries',
   standalone: true,
-  imports: [MatInputModule],
+  imports: [CommonModule, MatInputModule, CountryCardComponent],
   templateUrl: './countries.component.html',
   styleUrls: ['./countries.component.scss'],
 })
-export class CountriesComponent {}
+export class CountriesComponent {
+  countries$: Observable<Country[]> = this.countriesService.getAllCountries()
+  constructor(private countriesService: CountriesService) {}
+}
